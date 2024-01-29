3905b46070737863ec72a940c9ebae5c9c97ece3
Juschwy <jul.fehlmann@gmail.com>
2024-01-29 14:19:26 +0100

    Add files from react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import {Header} from "./components/header/Header.tsx";
+import {Display} from "./components/display/Display.tsx";
+import {Footer} from "./components/footer/Footer.tsx";
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <Display/>
+            <Footer/>
+        </>
+    )
+}
+
+export default App
