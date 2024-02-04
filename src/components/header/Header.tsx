f8b7e4973c54749de61b62c99ef30ddc71496210
Juschwy <jul.fehlmann@gmail.com>
2024-02-04 21:09:22 +0100

    Make logo navlink

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,10 +4,10 @@ import "./header.css"
 export function Header() {
     return (
         <header>
-            <div>
+            <NavLink id={"root-link"} to={"/"}>
                 <img src="/icons/skullredhat.png" alt="Logo"/>
                 <h1>JUSCHWY</h1>
-            </div>
+            </NavLink>
             <nav>
                 <NavLink to={"/"}>Start</NavLink>
                 <NavLink to={"/projects"}>Projects</NavLink>
